32052103ac78c2170b8a4e39b3e0d02512747050
Jade-Banares <proj.jade.banares@gmail.com>
2022-10-01 14:04:07 +0800

    configuration

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '../javascript/data_table';
-import '../dist/variables.css'
+import './assets/variables.css'
 
 
 createApp(App).mount('#app')
